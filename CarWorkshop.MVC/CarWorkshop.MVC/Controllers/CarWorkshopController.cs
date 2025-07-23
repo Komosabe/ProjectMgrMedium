1eff6b08794dce9688feadefa62ce4260a2165c6
Maciej Pietrasz <m.pietrasz@devcodi.pl>
2025-07-23 18:47:57 +0200

    fix version

--- CarWorkshop.MVC/CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -1,6 +1,4 @@
-﻿using CarWorkshop.Application.CarWorkshop;
-using CarWorkshop.Application.Services;
-using CarWorkshop.Domain.Entities;
+﻿using CarWorkshop.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarWorkshop.MVC.Controllers
@@ -14,13 +12,8 @@ namespace CarWorkshop.MVC.Controllers
             _carWorkshopService = carWorkshopService;
         }
 
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         [HttpPost]
-        public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
+        public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
         {
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create));
